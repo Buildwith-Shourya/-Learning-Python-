0de28b7143396edad50caf8ec8014177370ea4a7
shouryajeet02-bit <shouryajeet02@gmail.com>
2026-07-17 00:19:20 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,51 @@
+# Hello world printing 
+print("Hello world")
+ 
+# Comments -> if we place a hashtag(#) at the starting point of a line
+# and we don't want that comuter reads that line then we put comment(#)
+
+# Variables in Python
+
+name = "Shouryajeet singh"
+age = 18
+height = 5.8
+present = True 
+
+print("Name :" , name)
+print("Age :" , age)
+print("Height :" , height)
+print("Present :" , present )
+
+
+
+# Python Data Types
+
+name = "Shouryajeet"      # str
+age = 18              # int
+marks = 91.5          # float
+is_pass = True        # bool
+numbers = [1, 2, 3]   # list
+
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(is_pass))
+print(type(numbers)) 
+
+
+# Input and Output
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello,", name)
+print("Your age is", age)
+
+
+
+
+
+
+
+
+
