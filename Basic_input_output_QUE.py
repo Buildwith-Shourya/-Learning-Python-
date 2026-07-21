c8042571257e5bea383dcea9fb2d1ea2813840c6
shouryajeet02-bit <shouryajeet02@gmail.com>
2026-07-21 16:59:52 +0530

    Add Basic_input_output_QUE

---
+++ Basic_input_output_QUE.py
@@ -0,0 +1,42 @@
+# 1) Accept numbers from a user
+
+number = int(input(" Enter a number :- "))
+print(number)  
+# input = 50  ,  output = 50
+
+
+# 2) Display three string “Name”, “Is”, “James” as “Name**Is**James”
+
+string1 = "Name"
+string2 = "Is"
+string3 = "James"
+print(string1,string2,string3,sep="**")
+
+
+# 3) Display float number with 2 decimal places using print()
+num = 458.541315
+print(f"{num:.2f}")
+
+
+# 4) Sum of two integers 
+
+
+num1 = 50
+num2 = 40
+print(num1+num2)
+
+
+# 5) Accept two integers from user and print the sum 
+
+A = int(input(" Enter number "))
+B = int(input(" Enter number "))
+print(A+B)
+
+# 6) Accept the User's name, age and print in following manner
+  # Ex - Hello Shery, you are 12 years old. 
+
+name = input(" Enter name :-")
+age = int(input(" Enter age"))
+print(f"Hello{name},you are{age}years old.")
+
+
