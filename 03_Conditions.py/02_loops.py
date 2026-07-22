cecaf0077b0ea1ac5c086931b1e3140512af0d01
shouryajeet02-bit <shouryajeet02@gmail.com>
2026-07-23 03:30:33 +0530

    Add 03_ForLoops_QUE

--- 03_Conditions.py/02_loops.py
+++ 03_Conditions.py/02_loops.py
@@ -1,51 +1,61 @@
-# for loop
-#print number 0 - 20
-a = range(1,21,1)
-for i in a:
-    print(i)  
+# # for loop------------------------
+# #print number 0 - 20
+# a = range(1,21,1)
+# for i in a:
+#     print(i)  
 
-#take input and print table of it 
+# #take input and print table of it 
 
-a = int(input(" give number to print :- "))  
-for i in range(a,a*10+1,a):
-    print(i) 
+# a = int(input(" give number to print :- "))  
+# for i in range(a,a*10+1,a):
+#     print(i) 
 
 
-# Using length fungtion
+# # Using length fungtion
 
-name = "MITSUBISHI AC VERY POWERFULL"
-print(len(name)) 
+# name = "MITSUBISHI AC VERY POWERFULL"
+# print(len(name)) 
 
-#using other method
-Z = " SAVE WATER SAVE ENVIROMENT"
-for i in range(len(Z)):
+# #using other method
+# Z = " SAVE WATER SAVE ENVIROMENT"
+# for i in range(len(Z)):
+#     print(i)
+
+# # One more method
+# Q = " i am cool "
+# print(len(Q))
+
+# for i in range(len(Q)):
+#     print(Q[i])
+
+# #-----------------------------------------------------------------------
+# # break statement
+# #-----------------------------------------------------------------------
+
+# for i in range(5,55):
+#     if i == 26:
+#         break
+#     print(i)
+
+# #--------------------------------------------
+# #continue statement
+# #--------------------------------------------
+
+# for i in range(5,55):
+#     if i == 25:   # continue -> skips the current iteration of a loop and move to the next iteration
+#         continue  # without stopping the loop
+#     print(i)
+
+
+#work of else with break
+
+for i in range(1,21):
+    if i == 15:
+        print("break statement is executed")
+        break    
     print(i)
 
-# One more method
-Q = " i am cool "
-print(len(Q))
-
-for i in range(len(Q)):
-    print(Q[i])
-
-#-----------------------------------------------------------------------
-# break statement
-#-----------------------------------------------------------------------
-
-for i in range(5,55):
-    if i == 26:
-        break
-    print(i)
-
-#--------------------------------------------
-#continue statement
-#--------------------------------------------
-
-for i in range(5,55):
-    if i == 25:   # continue -> skips the current iteration of a loop and move to the next iteration
-        continue  # without stopping the loop
-    print(i)
-
-
-
-    
+else:    
+    print("break statement is not executed") 
+# if break not running then else  run 
+# if else not running then break run 
---
+++ 03_Conditions.py/03_ForLoops_QUE.py
@@ -0,0 +1,42 @@
+# #1 Accept an integer and Print hello world n times
+
+# n = int(input(" Enter your number :- "))
+# for i in range(n):
+#         print("Hello World")
+
+# #2 Print natural number up to n
+# n = int(input(" Enter your number :- "))
+# for i in range(1,n+1,1):
+#     print(i)
+
+
+# #3 Reverse for loop. Print n to 1
+# n = int(input(" Enter your number :- "))
+# for i in range(n,0,-1):
+#     print(i)
+
+
+# #4 Take a number as input and print its table
+# n = int(input(" Enter a number to print table :- "))
+# for i in range(1,11):
+#     print(f"{n} * {i} = {n*i}")
+
+# #5 Sum up to n terms
+# n = int(input(" Enter a number :- "))
+
+# sum = 0
+
+# for i in range(1,n+1):
+#     sum = sum + i
+# print(f"your sum is {sum}")    
+
+# #6 Factorial of a number
+# n = int(input(" Enter a number :- "))
+
+# fact = 1
+
+# for i in range(1,n+1):
+#     fact = fact * i
+# print(f" your factorial is {fact}")
+
+
