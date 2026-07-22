19c662de29b30aede7cf21b40bd56d68ee152042
shouryajeet02-bit <shouryajeet02@gmail.com>
2026-07-22 12:36:33 +0530

    Add 02_loops

---
+++ 03_Conditions.py/02_loops.py
@@ -0,0 +1,41 @@
+# # for loop
+# #print number 0 - 20
+# a = range(1,21,1)
+# for i in a:
+#     print(i)  
+
+# #take input and print table of it 
+
+# a = int(input(" give number to print :- "))  
+# for i in range(a,a*10+1,a):
+#     print(i) 
+
+
+# # Using length fungtion
+
+# name = "MITSUBISHI AC VERY POWERFULL"
+# print(len(name)) 
+
+# #using other method
+# Z = " SAVE WATER SAVE ENVIROMENT"
+# for i in range(len(Z)):
+#     print(i)
+
+# # One more method
+# Q = " i am cool "
+# print(len(Q))
+
+# for i in range(len(Q)):
+#     print(Q[i])
+
+#-----------------------------------------------------------------------
+# break statement
+#-----------------------------------------------------------------------
+
+for i in range(5,55):
+    if i == 26:
+        break
+    print(i)
+
+
+    
