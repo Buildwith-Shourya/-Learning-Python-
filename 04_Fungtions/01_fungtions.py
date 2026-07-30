f17bba995d24f2e8a9fa84df4c8d926458ce596e
shouryajeet02-bit <shouryajeet02@gmail.com>
2026-07-31 02:33:14 +0530

    Add 04_Fungtions

--- 04_Fungtions/01_fungtions.py
+++ 04_Fungtions/01_fungtions.py
@@ -12,19 +12,4 @@
 # addition(50,50) 
 
 
-def palindrome_checker(a):
-    copy = a 
-    rev = 0 
 
-    while a > 0:
-        rev = rev * 10 + a%10 
-        a = a //10 
-
-    if copy == rev:
-        print(f"{copy} is a palindrome number")
-    else:
-        print(f"{copy} is not a palindrome")
-
-palindrome_checker(121)
-palindrome_checker(456)
-palindrome_checker(324)
